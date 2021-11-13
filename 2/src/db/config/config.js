c71bcc4f31f643eb26296d157c28d6eaa7d4c0e2
prioarief <prioariefgunawangunawan@gmail.com>
2021-11-13 21:15:06 +0700

    update database config

--- 2/src/db/config/config.js
+++ 2/src/db/config/config.js
@@ -4,16 +4,9 @@ module.exports = {
 	development: {
 		username: process.env.DB_USER,
 		password: process.env.DB_PASS,
-		database: process.env.DB_NAME + '_development',
-		host: process.env.DB_HOST,
-		dialect: process.env.DB_DIALEC,
-		logging: false
-	},
-	production: {
-		username: process.env.DB_USER,
-		password: process.env.DB_PASS,
 		database: process.env.DB_NAME,
 		host: process.env.DB_HOST,
 		dialect: process.env.DB_DIALEC,
-	},
+		logging: false
+	}
 };
