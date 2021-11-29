712a3d2b17c18e3f0f5f4d13bd5988c9592c9573
prioarief <prioariefgunawangunawan@gmail.com>
2021-11-29 19:17:09 +0700

    add  if index of ) < index of (

--- 3/index.js
+++ 3/index.js
@@ -1,5 +1,6 @@
 const findFirstStringInBracket = (str) => {
 	if (!str.includes('(') || !str.includes(')')) return '';
+	if (str.indexOf(')') < str.indexOf('(')) return ''
 	return str.substring(str.indexOf('(') + 1, str.indexOf(')'));
 };
-console.log(findFirstStringInBracket('halo halooo (haloo)'));
+console.log(findFirstStringInBracket('saya )(test) lagi'));
